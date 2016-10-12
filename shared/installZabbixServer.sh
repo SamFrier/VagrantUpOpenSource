754fd721f82b4f8516cf31a83f06d7897343044b
Vagrant <vagrant@jirabot.qac.local>
2016-10-12 09:39:16 -0500

    changes to zabbix installer

--- shared/installZabbixServer.sh
+++ shared/installZabbixServer.sh
@@ -12,27 +12,25 @@ sudo useradd -g zabbix zabbix
 
 # install mysql on master
 sudo puppet apply /etc/puppet/modules/mysql/tests/init.pp
-sudo apt-get install -y mysql-client #libmysqlclient-dev libmysqld-dev # this probably renders the line above redundant
-#sudo cp /tmp/shared/modules/mysql/files/{install_mysql.sh,mysql-server_5.7.15-1ubuntu14.04_amd64.deb-bundle.tar} /opt/
-#sudo mv /opt/mysql-server_5.7.15-1ubuntu14.04_amd64.deb-bundle.tar /opt/mysql.tar
-#sudo ./install_mysql.sh
+sudo apt-get install -y mysql-client
 
 # create zabbix database
 sudo cp /tmp/shared/setupDatabase.sql /opt/
-sudo mysql -uroot -pvagrant < setupDatabase.sql
+sudo mysql -u root < setupDatabase.sql
 cd zabbix-3.2.1/database/mysql/
-sudo mysql -uzabbix -pvagrant zabbix < schema.sql
-sudo mysql -uzabbix -pvagrant zabbix < images.sql
-sudo mysql -uzabbix -pvagrant zabbix < data.sql
+sudo mysql -u zabbix zabbix < schema.sql
+sudo mysql -u zabbix zabbix < images.sql
+sudo mysql -u zabbix zabbix < data.sql
 
 # configure sources
 cd ../..
-sudo ./configure --enable-server --with-mysql #--with-net-snmp --with-libcurl --with-libxml2
+sudo ./configure --enable-server --with-mysql
 
 # install everything
 sudo make install
 
 # edit config files
+
 
 # start server daemon
 zabbix_server

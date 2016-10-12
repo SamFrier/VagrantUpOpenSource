e8d85896c9e3f3a0e9c6246cfc372a7e54680915
root <root@vumaster.qac.local>
2016-10-12 09:43:57 -0500

    zabbix tmp

---
+++ shared/modules/zabbix/install.pp
@@ -0,0 +1,23 @@
+class zabbix::install{
+
+file {'/opt/zabbixAgentInstall2.sh':
+ensure => present,
+owner => 'root',
+group => 'root',
+source => 'puppet:///modules/zabbix/zabbixAgentInstall2.sh',
+}
+
+file { '/opt/zabbix-3.2.1.tar.gz':
+ensure => present,
+owner => 'root',
+group => 'root',
+source => 'puppet:///modules/zabbix/zabbix-3.2.1.tar.gz',
+}
+
+exec { 'install_zabbix':
+provider => shell,
+command => '/opt/zabbixAgentInstall2.sh',
+require => [File['/opt/zabbixAgentInstall2.sh'], File['/opt/zabbix-3.2.1.tar.gz']],
+}
+}
+

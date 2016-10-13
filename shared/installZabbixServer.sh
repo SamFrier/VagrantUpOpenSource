34108aa7fcb808dfec76e4956cd49c625319c6b9
danielperryQA <daniel.perry@qa.com>
2016-10-13 17:14:47 +0100

    eidt

--- shared/installZabbixServer.sh
+++ shared/installZabbixServer.sh
@@ -4,7 +4,7 @@ echo "Installing Zabbix Server"
 
 # unzip zabbix server files
 cd /opt/
-sudo tar zxvf zabbix-3.2.1.tar.gz
+sudo tar zxf zabbix-3.2.1.tar.gz
 
 # create user account
 sudo groupadd zabbix
@@ -17,9 +17,9 @@ sudo puppet apply /etc/puppet/modules/mysql/tests/init.pp
 sudo cp /tmp/shared/setupDatabase.sql /opt/
 sudo mysql -u root < setupDatabase.sql
 cd zabbix-3.2.1/database/mysql/
-sudo mysql -u zabbix -p vagrantup zabbix < schema.sql
-sudo mysql -u zabbix -p vagrantup zabbix < images.sql
-sudo mysql -u zabbix -p vagrantup zabbix < data.sql
+sudo mysql -u zabbix -pvagrantup zabbix < schema.sql
+sudo mysql -u zabbix -pvagrantup zabbix < images.sql
+sudo mysql -u zabbix -pvagrantup zabbix < data.sql
 
 # configure sources
 cd ../..
@@ -37,6 +37,8 @@ sudo zabbix_server
 # setup apache web server and php
 sudo apt-get install -y apache2 apache2-doc apache2-utils
 sudo sed -i 's/KeepAlive On/KeepAlive Off/g' /etc/apache2/apache2.conf
+sudo apt-get install -y php5-mysql php5-gd
+
 sudo a2dissite 000-default.conf
 sudo service apache2 reload
 sudo apt-get install -y php5
@@ -45,6 +47,14 @@ sudo apt-get install -y php5
 sudo mkdir /var/www/zabbix
 cd frontends/php
 sudo cp -a . /var/www/zabbix
+
+sudo sed -i 's/max_size = 8/max_size = 16/g' /etc/php5/apache2/php.ini
+sudo sed -i 's/on_time = 30/on_time = 300/g' /etc/php5/apache2/php.ini
+sudo sed -i 's/max_input_time = 60/max_input_time = 300/g' /etc/php5/apache2/php.ini
+sudo sed -i 's/;date.timezone =/date.timezone = UTC/g' /etc/php5/apache2/php.ini
+
+sudo cp /tmp/shared/zabbix.conf.php /var/www/zabbix/conf/
+
 sudo service apache2 restart
 
 echo "Zabbix Server installed. Visit http://<server_ip>/zabbix to complete setup."
